b4eebc7342f746ed6c4bbb79cbaa26897c8df28a
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-11-18 23:36:26 +0000

    # CHEVRE-75 ### Changed - 予約の価格属性を価格仕様に変更

---
+++ src/factory/priceSpecificationType.ts
@@ -0,0 +1,7 @@
+import { priceSpecificationType } from '@chevre/factory';
+
+/**
+ * 価格仕様タイプ
+ */
+type PriceSpecificationType = priceSpecificationType;
+export default PriceSpecificationType;
--- src/factory/unitCode.ts
+++ src/factory/unitCode.ts
@@ -3,6 +3,14 @@
  */
 export enum UnitCode {
     /**
+     * no unit
+     */
+    C62 = 'C62',
+    /**
+     * 日
+     */
+    Day = 'DAY',
+    /**
      * 秒
      */
     Sec = 'SEC'
