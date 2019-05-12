fba4dbd04bde76adcc4a0add8ef17c3680e0286e
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-05-12 22:47:14 +0000

    ### Added - プロジェクトインターフェースを追加

--- src/factory/project.ts
+++ src/factory/project.ts
@@ -1,3 +1,35 @@
+import SortType from './sortType';
+
+export interface ISettings {
+    chevre?: {
+        endpoint: string;
+    };
+    coa?: {
+        endpoint: string;
+        refreshToken: string;
+    };
+    gmo?: {
+        endpoint: string;
+        siteId: string;
+        sitePass: string;
+    };
+    mvtkReserve?: {
+        endpoint: string;
+    };
+    pecorino?: {
+        endpoint: string;
+    };
+    waiter?: {
+        disabled: boolean;
+        endpoint: string;
+        secret: string;
+    };
+    lineNotify?: {
+        accessToken: string;
+        endpoint: string;
+    };
+}
+
 /**
  * プロジェクトインターフェース
  */
@@ -10,4 +42,26 @@ export interface IProject {
     name?: string;
     telephone?: string;
     url?: string;
+    settings?: ISettings;
+}
+
+/**
+ * ソート条件インターフェース
+ */
+export interface ISortOrder {
+    _id?: SortType;
+}
+
+/**
+ * プロジェクト検索条件インターフェース
+ */
+export interface ISearchConditions {
+    limit?: number;
+    page?: number;
+    sort?: ISortOrder;
+    ids?: string[];
+    /**
+     * 名称
+     */
+    name?: string;
 }
