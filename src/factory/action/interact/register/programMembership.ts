5737f61501af686dfec6101b29ddf7fc5ea738b8
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-08-26 01:05:37 +0000

    ### Changed - 会員登録登録アクションを検索しやすいように調整

--- src/factory/action/interact/register/programMembership.ts
+++ src/factory/action/interact/register/programMembership.ts
@@ -1,18 +1,20 @@
-import { IAcceptedOffer } from '../../../order';
+import { IAcceptedOffer as IOrderAcceptedOffer } from '../../../order';
 import { IProgramMembership } from '../../../programMembership';
 import * as RegisterActionFactory from '../register';
 
-/**
- * 会員プログラム登録アクションの対象は、受け入れられた会員プログラムオファー
- */
-export type IObject = IAcceptedOffer<IProgramMembership>;
+export type IAcceptedOffer = IOrderAcceptedOffer<IProgramMembership>;
+export type IObject = IAcceptedOffer | IProgramMembership;
+
 export type IResult = any;
+
 // tslint:disable-next-line:no-empty-interface
 export interface IPotentialActions {
 }
+
 export interface IAttributes extends RegisterActionFactory.IAttributes<IObject, IResult> {
     potentialActions?: IPotentialActions;
 }
+
 /**
  * 会員プログラム登録アクションインターフェース
  */
