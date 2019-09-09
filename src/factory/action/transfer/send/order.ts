4234db3b2179fc71f936d714d4713553029ef487
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-09-09 23:11:35 +0000

    ### Changed - 注文配送時、返金時のメール送信タスクをリストで登録できるように調整

--- src/factory/action/transfer/send/order.ts
+++ src/factory/action/transfer/send/order.ts
@@ -33,7 +33,7 @@ export interface IPotentialActions {
     /**
      * 注文配送を通知するEメール送信アクション
      */
-    sendEmailMessage?: ISendEmailMessageActionAttributes;
+    sendEmailMessage?: ISendEmailMessageActionAttributes | ISendEmailMessageActionAttributes[];
 }
 
 export interface IAttributes extends SendActionFactory.IAttributes<IObject, IResult> {
