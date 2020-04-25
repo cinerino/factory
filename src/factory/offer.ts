580d1d177655cb6b7ed9023c8cd46bdaacf95d72
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-04-25 10:33:52 +0000

    ### Changed - update @chevre/factory

--- src/factory/offer.ts
+++ src/factory/offer.ts
@@ -1,7 +1,5 @@
 import * as chevre from '../chevre';
 import * as SeatReservationOfferFactory from './offer/seatReservation';
-import { IPriceSpecification } from './priceSpecification';
-import PriceSpecificationType from './priceSpecificationType';
 import { Identifier as WebAPIIdentifier, IService as IWebAPI } from './service/webAPI';
 
 export import seatReservation = SeatReservationOfferFactory;
@@ -16,13 +14,8 @@ export type IOfferedThrough = IWebAPI<WebAPIIdentifier>;
  * @see https://schema.org/Offer
  */
 export interface IOffer extends chevre.offer.IOffer {
-    identifier?: any;
     /**
      * オファー供給サービス
      */
     offeredThrough?: IOfferedThrough;
-    /**
-     * One or more detailed price specifications, indicating the unit price and delivery or payment charges.
-     */
-    priceSpecification?: IPriceSpecification<PriceSpecificationType>;
 }
--- src/factory/priceSpecification.ts
+++
@@ -1,8 +0,0 @@
-import * as chevre from '../chevre';
-import PriceSpecificationType from './priceSpecificationType';
-
-/**
- * 価格仕様インターフェース
- */
-export type IPriceSpecification<T extends PriceSpecificationType> = chevre.priceSpecification.IPriceSpecification<T> & {
-};
