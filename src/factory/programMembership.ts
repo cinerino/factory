59c759bcf91b71373c00e08d590988f19e6d569e
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2021-05-08 06:24:45 +0000

    - update @chevre/factory

--- src/factory/programMembership.ts
+++
@@ -1,7 +0,0 @@
-import * as chevre from '../chevre';
-
-export type IMember = chevre.person.IPerson;
-
-export interface IProgramMembership extends chevre.programMembership.IProgramMembership {
-    member?: IMember[];
-}
